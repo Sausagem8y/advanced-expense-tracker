f366d723a6d527f3a59a70b27153b09010ec173b
Sausagem8y <87502439+Sausagem8y@users.noreply.github.com>
2022-12-08 19:23:02 -0700

    Add files via upload

---
+++ screens/RecentExpenses.js
@@ -0,0 +1,50 @@
+import { useContext, useEffect, useState } from "react";
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+import ErrorOverlay from "../components/UI/ErrorOverlay";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
+import { ExpensesContext } from "../store/ExpenseContext";
+import { getDateMinusDays } from "../util/date";
+import { fetchExpenses } from "../util/http";
+
+function RecentExpenses() {
+  const [isFetching, setIsFetching] = useState(true);
+  const [error, setError] = useState();
+
+  const expensesCtx = useContext(ExpensesContext);
+
+  useEffect(() => {
+    async function getExpenses() {
+      setIsFetching(true);
+      try {
+        const expenses = await fetchExpenses();
+        expensesCtx.setExpenses(expenses);
+      } catch (error) {
+        setError("Could not fetch Expenses!");
+      }
+      setIsFetching(false);
+    }
+
+    getExpenses();
+  }, []);
+
+  if (error && !isFetching) {
+    return <ErrorOverlay message={error} />;
+  }
+
+  if (isFetching) {
+    return <LoadingOverlay />;
+  }
+
+  const recentExpenses = expensesCtx.expenses.filter((expense) => {
+    const today = new Date();
+    const date7DaysAgo = getDateMinusDays(today, 7);
+
+    return expense.date > date7DaysAgo;
+  });
+
+  return (
+    <ExpensesOutput expenses={recentExpenses} expensesPeriod="Last 7 Days" />
+  );
+}
+
+export default RecentExpenses;
